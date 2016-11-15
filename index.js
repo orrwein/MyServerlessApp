b808dadf7fce51db15634b33f64601f97a6049be
orrwein <orrwein@gmail.com>
2016-11-14 17:33:26 -0800

    fix

--- index.js
+++ index.js
@@ -1,68 +1,12 @@
-'use strict';
-console.log('Loading function');
+exports.handler = (event, context, callback) => {
 
-let doc = require('dynamodb-doc');
-let dynamo = new doc.DynamoDB();
+    var time = require('time');
+    var currentTime = new time.Date();
 
-const tableName = process.env.TABLE_NAME;
+    currentTime.setTimezone("America/Los_Angeles");
 
-const createResponse = (statusCode, body) => {
-    return {
-        "statusCode": statusCode,
-        "body": body
-    }
-};
-
-exports.get = (event, context, callback) => {
-    var params = {
-        "TableName": tableName,
-        "Key": {
-            id: event.pathParameters.resourceId
-        }
-    };
-    dynamo.getItem(params, (err, data) => {
-        var response;
-        if (err)
-            response = createResponse(500, err);
-        else
-            response = createResponse(200, data.Item.doc);
-        callback(null, response);
-    });
-};
-
-exports.put = (event, context, callback) => {
-    var item = {
-        "id": event.pathParameters.resourceId,
-        "doc": event.body
-    };
-
-    var params = {
-        "TableName": tableName,
-        "Item": item
-    };
-    dynamo.putItem(params, (err, data) => {
-        var response;
-        if (err)
-            response = createResponse(500, err);
-        else
-            response = createResponse(200, null);
-        callback(null, response);
-    });
-};
-
-exports.delete = (event, context, callback) => {
-    var params = {
-        "TableName": tableName,
-        "Key": {
-            "id": event.pathParameters.resourceId
-        }
-    };
-    dynamo.deleteItem(params, (err, data) => {
-        var response;
-        if (err)
-            response = createResponse(500, err);
-        else
-            response = createResponse(200, null);
-        callback(null, response);
+     callback(null, {
+        statusCode: '200',
+        body: currentTime.toString(),
     });
 };
