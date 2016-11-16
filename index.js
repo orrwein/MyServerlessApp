fc5d74fbddb62481bdab4bffab7820fc180e92cb
orrwein <orrwein@gmail.com>
2016-11-15 20:03:31 -0800

    A

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 exports.handler = (event, context, callback) => {
 
-    var time = require('time');
-    var currentTime = new time.Date();
+    //var time = require('time');
+    //var currentTime = new time.Date();
 
-    currentTime.setTimezone("America/Los_Angeles");
+    //currentTime.setTimezone("America/Los_Angeles");
 
-    callback(null, {
+    /*callback(null, {
         statusCode: '200',
         body: '<h1 style="color:#000080;">' + currentTime.toString() + '</h1>',
         headers: {
                   'Content-Type': 'text/html; charset=utf-8',
     },
-    });
+  });*/
+  callback(null, {
+      statusCode: '200',
+      body: '<h1 style="color:#000080;">' + 'My test!' + '</h1>',
+      headers: {
+                'Content-Type': 'text/html; charset=utf-8',
+  },
+});
 };
