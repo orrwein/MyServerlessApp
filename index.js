e68196c239bdb08e27cc8c8229eec5f858e28aaf
orrwein <orrwein@gmail.com>
2016-11-19 13:18:24 -0800

    s

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 exports.handler = (event, context, callback) => {
 
-    //var time = require('time');
+    /*var time = require('time');
     //var currentTime = new time.Date();
 
     // Replace with env var ("America/Los_Angeles" and "America/New_York")
-    //var timeZone = process.env.TIME_ZONE;
-    //currentTime.setTimezone(timeZone);
+    var timeZone = process.env.TIME_ZONE;
+    currentTime.setTimezone(timeZone);
 
-    /*callback(null, {
+    callback(null, {
         statusCode: '200',
         body: '<h1 style="color:#000080;">' + 'The time in ' + timeZone +  ' is: ' + currentTime.toString() + '</h1>',
         headers: {
@@ -16,7 +16,7 @@ exports.handler = (event, context, callback) => {
   });*/
   callback(null, {
       statusCode: '200',
-      body: '<h1 style="color:#000080;">' + 'My test!' + '</h1>',
+      body: '<h1 style="color:#000080;">' + process.env.TIME_ZONE + '</h1>',
       headers: {
                 'Content-Type': 'text/html; charset=utf-8',
   },
