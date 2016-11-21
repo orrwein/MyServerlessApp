b98ba4935e6511688f7a90073fb81d0542e6c1be
orrwein <orrwein@gmail.com>
2016-11-21 13:28:46 -0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
 
-    var time = require('time');
+    /*var time = require('time');
     var currentTime = new time.Date();
 
     currentTime.setTimezone(process.env.TIME_ZONE);
@@ -8,16 +8,16 @@ exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
         body: '<h1 style="color:#000080;">' + 'Welcome to my ' + process.env.STAGE + ' stage!<br/>' +
-              'The time in ' + process.env.TIME_ZONE +  ' is: ' + currentTime.toString() + '</h1>',
+              'The time in ' + process.env.TIME_ZONE + ' is: ' + currentTime.toString() + '</h1>',
         headers: {
                   'Content-Type': 'text/html; charset=utf-8',
     },
-  });
-  /*callback(null, {
+  });*/
+  callback(null, {
       statusCode: '200',
       body: '<h1 style="color:#000080;">' + 'Welcome tossss my ' + process.env.STAGE + ' stage!</h1>',
       headers: {
                 'Content-Type': 'text/html; charset=utf-8',
   },
-});*/
+});
 };
